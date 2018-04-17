e08fb75547cbf17d76bef65b7cc500efdc823031
Josh LaMar <jolamar@iu.edu>
2018-04-17 08:37:29 -0600

    better demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,10 +5,18 @@ import { Alert, Button } from '../../src/components'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>rivet-react-nwb Demo</h1>
-      <Button>Hello</Button>
-      <Alert>ALERT!</Alert>'
+    return <div className="rvt-container rvt-container--freshman m-top-md">
+      <h1>Rivet 1.0.0 React Components Demo</h1>
+
+      <Alert className="m-top-md" clickDismiss={function(){console.log('dismissed alert')}}>Base</Alert>
+      <Alert error className="m-top-md" clickDismiss={function(){console.log('dismissed alert')}}>Error</Alert>
+      <Alert info className="m-top-md" clickDismiss={function(){console.log('dismissed alert')}}>Info</Alert>
+      <Alert success className="m-top-md" clickDismiss={function(){console.log('dismissed alert')}}>Success</Alert>
+
+      <Button className="button m-top-md">Hello</Button>
+      <Button className="button m-left-md button--secondary m-top-lg">World</Button>
+
+
     </div>
   }
 }
