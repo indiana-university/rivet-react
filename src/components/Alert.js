5345c4b1f3db8cef829ee0f125946a9baf2d3da9
Josh LaMar <jolamar@iu.edu>
2018-05-10 13:32:56 -0600

    adds react dismiss alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,6 +4,18 @@ import classNames from 'classnames'
 
 // Longhand
 export class Alert extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: true
+        };
+    }
+
+    hideAlert() {
+        this.setState({ visible: false })
+    }
+
     render() {
         var props = copy(this.props)
         var c = []
@@ -44,7 +56,7 @@ export class Alert extends Component {
         var dismiss
         if (props.clickDismiss) {
             dismiss =
-                <button className="rvt-alert__dismiss" onClick={props.clickDismiss}>
+                <button className="rvt-alert__dismiss" onClick={()=>{ props.clickDismiss(); this.hideAlert(); }}>
                     <span className="rvt-sr-only">Dismiss this alert</span>
                     <svg role="img" alt="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                         <path fill="currentColor" d="M9.41,8l5.29-5.29a1,1,0,0,0-1.41-1.41L8,6.59,2.71,1.29A1,1,0,0,0,1.29,2.71L6.59,8,1.29,13.29a1,1,0,1,0,1.41,1.41L8,9.41l5.29,5.29a1,1,0,0,0,1.41-1.41Z"/>
@@ -52,12 +64,16 @@ export class Alert extends Component {
                 </button>
         }
 
+        let visible = this.state.visible;
+
         return (
+            visible &&
             <div className={classNames(c)} id={props.id} role='alertdialog' aria-labelledby={titleid}>
                 {header}
                     <div className='rvt-alert__message'>{props.children}</div>
                 {dismiss}
             </div>
+
         )
 
     }
