802209b76aef0cfcbedd6d63b56eafff4d7702fe
John Hoerr <jhoerr@gmail.com>
2018-07-09 09:31:56 -0400

    Implement text input (#9)
    * Implement text input
    * Exclude 'index.ts' files from code coverage.

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,68 @@
+
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import * as Rivet from '../Rivet';
+import Input from './Input';
+
+describe('<Input />', () => {
+    describe("Rendering", () => {
+        it('should render without throwing an error', () => {
+            const cut = shallow(<Input label="Label" />);
+            expect(cut.find('input')).toHaveLength(1);
+        });
+        it('should have the correct type', () => {
+            const cut = shallow(<Input label="Label" />);
+            expect(cut.find('input').prop('type')).toEqual("text");
+        });
+        it('should render the label', () => {
+            const cut = shallow(<Input label="Label" />);
+            expect(cut.find('label').text()).toEqual("Label");
+        });
+        it('should associate the label with the input', () => {
+            const cut = shallow(<Input id="the_id" label="Label" />);
+            expect(cut.find('input').prop('id')).toEqual("the_id");
+            expect(cut.find('label').prop('htmlFor')).toEqual("the_id");
+        });
+        it('should apply custom class', () => {
+            const cut = shallow(<Input className="my-class" label="Label" />);
+            expect(cut.find('div.rvt-input').hasClass("my-class")).toEqual(true);
+        });
+    });
+    describe("Notes", () => {
+        it('should apply the note', () => {
+            const cut = shallow(<Input note="Note" label="Label" />);
+            expect(cut.find('small').text()).toEqual("Note");
+        });
+        it('should associate the input with the note', () => {
+            const cut = shallow(<Input id="the_id" note="Note" label="Label" />);
+            expect(cut.find('input').prop('aria-describedby')).toEqual("the_id_note");
+        });
+    });
+    describe("Inline Alerts", () => {
+        it('info style', () => { 
+            const cut = shallow(<Input info note="Note" label="Label" />);
+            expect(cut.find('input').hasClass("rvt-has-info")).toEqual(true);
+            expect(cut.find('.rvt-inline-alert').hasClass("rvt-inline-alert--has-info")).toBe(true);
+        });
+        it('valid style', () => { 
+            const cut = shallow(<Input valid note="Note" label="Label" />);
+            expect(cut.find('input').hasClass("rvt-is-valid")).toEqual(true);
+            expect(cut.find('.rvt-inline-alert').hasClass("rvt-inline-alert--is-valid")).toBe(true);
+        });
+        it('warning style', () => { 
+            const cut = shallow(<Input warning note="Note" label="Label" />);
+            expect(cut.find('input').hasClass("rvt-has-warning")).toEqual(true);
+            expect(cut.find('.rvt-inline-alert').hasClass("rvt-inline-alert--has-warning")).toBe(true);
+        });
+        it('invalid style', () => { 
+            const cut = shallow(<Input invalid note="Note" label="Label" />);
+            expect(cut.find('input').hasClass("rvt-is-invalid")).toEqual(true);
+            expect(cut.find('input').prop("aria-invalid")).toEqual(true);
+            expect(cut.find('.rvt-inline-alert').hasClass("rvt-inline-alert--is-invalid")).toBe(true);
+        });
+        it('invalid has aria-invalid flag', () => { 
+            const cut = shallow(<Input invalid note="Note" label="Label" />);
+            expect(cut.find('input').prop("aria-invalid")).toEqual(true);
+        });
+    });
+});
