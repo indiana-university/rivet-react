ff13b6beff4c19fb41f79c6ef08da2e69b860a5e
John Hoerr <jhoerr@gmail.com>
2018-07-10 14:56:49 -0400

    Implement ordered/unordered lists. (#11)
    * Implement ordered and unordered lists.
    * Exclude index.tsx from coverage.
    * Only gather code coverage from `/src/components`.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,4 +3,5 @@ export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Footer } from './Footer';
 export { Input, Textarea } from './Input';
+export { default as List } from './List';
 export { Action, Nav } from './Rivet';
