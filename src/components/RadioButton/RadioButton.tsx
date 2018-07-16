51f0464d121c824249649900261c265c86d37a9b
Francis Fernandez <fraferna@iu.edu>
2018-07-16 14:27:10 -0400

    Convert Radio Button to presentational component
    Co-Authored-By: Andy Hill <athill@users.noreply.github.com>

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,24 +1,27 @@
-import * as React from 'react'
-import * as Rivet from '../Rivet'
+import * as React from 'react';
+import * as Rivet from '../Rivet';
 
 interface RadioButtonProps extends Rivet.Props {
     /** The text label/description for this radio option */
-    label: string,
-}
+    label: string
+};
 
 const componentClass = "rvt-radio";
 
-export class RadioButton extends React.PureComponent<RadioButtonProps & React.InputHTMLAttributes<HTMLInputElement>> {
-    public render() {
-        const { id, label, name, ...attrs} = this.props;
-        const inputId = id || Rivet.shortuid();
-        return (
-            <React.Fragment>
-                <input id={inputId} type="radio" className={Rivet.classify(this.props, componentClass)} {...attrs} />
-                <label htmlFor={inputId}>{label}</label>
-            </React.Fragment>
-        );
-    }
-}
+export const RadioButton : React.SFC < RadioButtonProps & React.InputHTMLAttributes < HTMLInputElement >> = ({
+    id = Rivet.shortuid(),
+    label,
+    ...attrs
+}) => (
+    <React.Fragment>
+        <input
+            id={id}
+            type="radio"
+            className={Rivet.classify(attrs, componentClass)}
+            {...attrs}/>
+        <label htmlFor={id}>{label}</label>
+    </React.Fragment>
+);
+RadioButton.displayName = 'RadioButton';
 
 export default RadioButton
