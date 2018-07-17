7ee391a7adf5ab4fe5530b750525a5cb8feebb72
James Bennett <jawbenne@iu.edu>
2018-07-17 10:20:09 -0400

    Switch Button tests from shallow rendering to mount
    Button is now wrapped in a higher-order component so shallow rendering does not work as well as it used to since it is no longer rendering the Button component.  This switches the tests to mounting the component which handles rendering the entire tree instead.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ import * as util from '../util'
 /**
  * The properties of a button.
  */
-interface ButtonProps extends Rivet.Props {
+interface ButtonProps {
     onClick?: Rivet.Action,
     /** Optional Rivet style: a success/danger/plain button. See: https://rivet.uits.iu.edu/components/forms/buttons/#button-examples */
     rvtStyle?: "success" | "danger" | "plain" | "default"
