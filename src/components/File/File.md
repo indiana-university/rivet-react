e4b04f5bc7feb717b3f03baff69c7cd93920f318
James Bennett <jawbenne@iu.edu>
2018-07-25 15:07:49 -0400

    Convert File component to TypeScript
    Converts the File component to TypeScript and adds tests

---
+++ src/components/File/File.md
@@ -0,0 +1,4 @@
+### Sample
+```jsx
+<File name="demo" />
+```
---
+++ src/components/File/File.test.tsx
@@ -0,0 +1,55 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+import File, { FileState, UnwrappedFile } from './File';
+
+const simulatedChangeEvent = {
+    target: {
+        files: [
+            { name: 'foo.txt' },
+            { name: 'bar.txt' }
+        ]
+    }
+};
+
+describe('<File />', () => {
+    describe('Rendering and styling', () =>{
+        it('should render without throwing an error', () => {
+            const cut = mount(<File />);
+            expect(cut.find('input')).toHaveLength(1);
+        });
+        it('should pass attributes through', () => {
+            const cut = mount(<File id="the_id" />);
+            expect(cut.find('input').prop('id')).toEqual('the_id');
+        });
+        it('should generate an ID if one is not provided', () => {
+            const cut = mount(<File />);
+            expect(cut.find('input').prop('id')).toBeDefined();
+        });
+        it('should apply custom classes to the wrapper div', () => {
+            const cut = mount(<File className="foo" />);
+            expect(cut.find('div.rvt-file').length).toBe(1);
+            expect(cut.find('div.foo').length).toBe(1);
+        });
+        it('should allow for a custom label', () => {
+            const cut = mount(<File label="Attach File" />);
+            expect(cut.find('label').text()).toBe('Attach File');
+        });
+    });
+
+    describe('Stateful component behavior', () => {
+        it('should handle file selection', () => {
+            const cut = mount(<UnwrappedFile />);
+            let state = cut.state() as FileState;
+            expect(state.files).toBe('');
+            expect(cut.find('input').simulate('change', simulatedChangeEvent));
+            state = cut.state() as FileState;
+            expect(state.files).toBe('foo.txt, bar.txt');
+        });
+        it('should invoke a provided onChange handler', () => {
+            const spy = jest.fn();
+            const cut = mount(<UnwrappedFile onChange={spy} />);
+            expect(cut.find('input').simulate('change', simulatedChangeEvent));
+            expect(spy).toBeCalled();
+        });
+    });
+});
