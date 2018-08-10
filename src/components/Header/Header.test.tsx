041f968bb86ce8a7871bdfdbaef8f72653d52e8b
John Hoerr <jhoerr@gmail.com>
2018-08-10 15:34:31 -0400

    Header with title

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,20 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import Header from './Header';
+
+describe('<Grid />', () => {
+    let cut;
+    
+    beforeEach(() => {
+        cut = shallow(<Header title="foo"/>);
+    })
+
+    describe('Rendering and styling', () =>{
+        it('should render without throwing an error', () => {
+            expect(cut.find('header')).toHaveLength(1);
+        });
+        it('should include the title', () => {
+            expect(cut.find('span.rvt-header__title > a').text()).toEqual("foo");
+        });
+    });
+});
