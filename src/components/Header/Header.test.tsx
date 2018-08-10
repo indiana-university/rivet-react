a2d9404b215cf02abdbbf6611ec9ab9f25809076
James Bennett <jawbenne@iu.edu>
2018-08-10 17:08:52 -0400

    Add a children test for the header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -16,5 +16,15 @@ describe('<Header />', () => {
         it('should include the title', () => {
             expect(cut.find('span.rvt-header__title > a').text()).toEqual("foo");
         });
+        it('should render children in a container div', () => {
+            cut = shallow(
+                <Header title="foo">
+                    <Header.Navigation>
+                        <a href="#">Example</a>
+                    </Header.Navigation>
+                </Header>
+            );
+            expect(cut.find('div.rvt-header__controls').find('a')).toHaveLength(1);
+        });
     });
 });
