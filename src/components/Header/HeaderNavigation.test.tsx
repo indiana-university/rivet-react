1ca908701be4a64025da2c0b6fc61957b989c66f
James Bennett <jawbenne@iu.edu>
2018-08-20 09:45:42 -0400

    Fix HeaderNavigation tests

--- src/components/Header/HeaderNavigation.test.tsx
+++ src/components/Header/HeaderNavigation.test.tsx
@@ -1,25 +1,27 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import * as React from 'react';
 import Header from './Header';
 
-describe('<Navigation />', () => {
+describe('<HeaderNavigation />', () => {
     let cut;
     
     beforeEach(() => {
-        cut = shallow(
-            <Header.Navigation>
-                <a href="#">Foo</a>
-                <button>Bar</button>
-            </Header.Navigation>
+        cut = mount(
+            <Header title="Testing">
+                <Header.Navigation>
+                    <a href="#">Foo</a>
+                    <button>Bar</button>
+                </Header.Navigation>
+            </Header>
         );
     })
 
     describe('Rendering and styling', () =>{
         it('should render without throwing an error', () => {
-            expect(cut.find('nav')).toHaveLength(1);
+            expect(cut.find('nav.rvt-header__main-nav')).toHaveLength(1);
         });
         it('should include the list items', () => {
-            expect(cut.find('nav > ul > li')).toHaveLength(2);
+            expect(cut.find('nav.rvt-header__main-nav > ul > HeaderNavigation > li')).toHaveLength(2);
         });
     });
 });
