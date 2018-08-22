4d4f5bfbc3709e21c217d0ba5f49aeff7b259685
John Hoerr <jhoerr@gmail.com>
2018-08-22 13:21:53 -0400

    proposed refactor
    * remove 'default' options.
    * don't destructure styling props
    * pass `attrs` (with styling props) to styling functions

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,36 +8,35 @@ import * as Rivet from '../util/Rivet';
  */
 export interface ButtonProps {
     /** Optional Rivet style: a success/danger/plain button. The 'navigation' variant is intended to support the Header component only. See: https://rivet.uits.iu.edu/components/forms/buttons/#button-examples */
-    variant?: 'success' | 'danger' | 'plain' | 'default' | 'navigation';
     /** Optional Rivet style: a small button. See: https://rivet.uits.iu.edu/components/forms/buttons/#small-buttons */
-    size?: 'small' | 'default';
     /** Optional Rivet style: a secondary button. See: https://rivet.uits.iu.edu/components/forms/buttons/#secondary-variations */
-    role?: 'secondary' | 'default';
+    variant?: 'success' | 'danger' | 'plain' | 'navigation';
+    size?: 'small';
+    role?: 'secondary';
     innerRef?: React.Ref<HTMLButtonElement>;
 }
 
 export const buttonPropTypes = {
-  variant: PropTypes.oneOf(['success', 'danger', 'plain', 'default', 'navigation']),
-  size: PropTypes.oneOf(['small', 'default']),
-  role: PropTypes.oneOf(['secondary', 'default']),
+  variant: PropTypes.oneOf(['success', 'danger', 'plain', 'navigation']),
+  size: PropTypes.oneOf(['small']),
+  role: PropTypes.oneOf(['secondary']),
   innerRef: PropTypes.node
 }
 
-const None = '';
 const buttonClass = 'rvt-button';
 /**
  * Generate the combined role and variation class for this button, if applicable.
  * @param attrs This button's properties
  * @see https://rivet.uits.iu.edu/components/forms/buttons/#secondary-variations
  */
-const buttonRoleAndStyle = ({ role, variant }) => {
-  if(variant === 'navigation') {
+const buttonRoleAndStyle = (props: ButtonProps) => {
+  if(props.variant === 'navigation') {
     return 'rvt-dropdown__toggle';
   }
   const classParts = [
-    variant !== 'default' ? variant : None,
-    role !== 'default' ? role : None
-  ].filter(x => x !== None);
+    props.variant,
+    props.role
+  ].filter(x => x !== undefined);
   // combine variation and style, if any.
   return classParts.length === 0
     ? buttonClass
@@ -49,11 +48,11 @@ const buttonRoleAndStyle = ({ role, variant }) => {
  * @param attrs This button's properties
  * @see https://rivet.uits.iu.edu/components/forms/buttons/#small-buttons
  */
-const buttonSize = ({ size }) => (size !== "default" ? `${buttonClass}--${size}` : None);
+const buttonSize = (props: ButtonProps) => (props.size ? `${buttonClass}--${props.size}` : undefined);
 
 const Button: React.SFC<ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>> = 
-  ({role = "default", size = "default", variant = "default", onClick, id = Rivet.shortuid(), innerRef, className, children, ...attrs}) => {
-  const classes = classNames(buttonRoleAndStyle({ role, variant }), buttonSize({ size }), className);
+  ({onClick, id = Rivet.shortuid(), innerRef, className, children, ...attrs}) => {
+  const classes = classNames(buttonRoleAndStyle(attrs), buttonSize(attrs), className);
   return (
     <button
       id={id}
