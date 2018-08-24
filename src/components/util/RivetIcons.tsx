90f4e7a9844ad61fb17a0a5a0811064536cdac53
John Hoerr <jhoerr@gmail.com>
2018-08-24 12:53:00 -0400

    Formatting

--- src/components/util/RivetIcons.tsx
+++ src/components/util/RivetIcons.tsx
@@ -5,7 +5,6 @@ export const svg16 = (className: string = "", node: React.ReactNode) =>
         {node}
     </svg>
 
-
 /****************
  * Interactions 
  ****************/
@@ -15,18 +14,15 @@ export const caretDown = (className?: string) =>
         <path fill="currentColor" d="M8,12.46a2,2,0,0,1-1.52-.7L1.24,5.65a1,1,0,1,1,1.52-1.3L8,10.46l5.24-6.11a1,1,0,0,1,1.52,1.3L9.52,11.76A2,2,0,0,1,8,12.46Z" />
     );
 
-
 export const close = (className?: string) =>
     svg16(className,
         <path fill="currentColor" d="M9.41,8l5.29-5.29a1,1,0,0,0-1.41-1.41L8,6.59,2.71,1.29A1,1,0,0,0,1.29,2.71L6.59,8,1.29,13.29a1,1,0,1,0,1.41,1.41L8,9.41l5.29,5.29a1,1,0,0,0,1.41-1.41Z" />
     );
 
-
 export const file = (className?: string) =>
     svg16(className,
         <path fill="currentColor" d="M10.41,1H3.5A1.3,1.3,0,0,0,2.2,2.3V13.7A1.3,1.3,0,0,0,3.5,15h9a1.3,1.3,0,0,0,1.3-1.3V4.39ZM11.8,5.21V6H9.25V3h.34ZM4.2,13V3h3V6.75A1.25,1.25,0,0,0,8.5,8h3.3v5Z" />
     );
-
 
 export const menu = (className?: string) =>
     svg16(className,
@@ -35,8 +31,7 @@ export const menu = (className?: string) =>
             <path d="M15,9H1A1,1,0,0,1,1,7H15a1,1,0,0,1,0,2Z" />
             <path d="M15,15H1a1,1,0,0,1,0-2H15a1,1,0,0,1,0,2Z" />
         </g>
-    )
-
+    );
 
 /****************
  * Alerts 
@@ -49,7 +44,7 @@ export const info = (className?: string) =>
             <path d="M8,12a1,1,0,0,1-1-1V8A1,1,0,0,1,9,8v3A1,1,0,0,1,8,12Z" />
             <circle cx="8" cy="5" r="1" />
         </g>
-    )
+    );
 
 export const success = (className?: string) =>
     svg16(className,
@@ -57,7 +52,7 @@ export const success = (className?: string) =>
             <path d="M10.2,5.4,7.1,9.53,5.67,8.25a1,1,0,1,0-1.34,1.5l2.05,1.82a1.29,1.29,0,0,0,.83.32h.12a1.23,1.23,0,0,0,.88-.49L11.8,6.6a1,1,0,1,0-1.6-1.2Z" />
             <path d="M8,0a8,8,0,1,0,8,8A8,8,0,0,0,8,0ZM8,14a6,6,0,1,1,6-6A6,6,0,0,1,8,14Z" />
         </g>
-    )
+    );
 
 export const warning = (className?: string) =>
     svg16(className,
@@ -65,7 +60,7 @@ export const warning = (className?: string) =>
             <path d="M11,9H5A1,1,0,0,1,5,7h6a1,1,0,0,1,0,2Z" />
             <path d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,2a6,6,0,1,0,6,6A6,6,0,0,0,8,2Z" />
         </g>
-    )
+    );
 
 export const error = (className?: string) =>
     svg16(className,
@@ -91,4 +86,3 @@ export const iuTridentFooter =
         <title id="footer-trident">IU Trident Logo</title>
         <polygon points="13.33 3.32 13.33 5.21 14.76 5.21 14.76 15.64 11.9 15.64 11.9 1.9 13.33 1.9 13.33 0 6.67 0 6.67 1.9 8.09 1.9 8.09 15.64 5.24 15.64 5.24 5.21 6.67 5.21 6.67 3.32 0 3.32 0 5.21 1.43 5.21 1.43 17.47 3.7 19.91 8.09 19.91 8.09 22.76 6.67 22.76 6.67 25.13 13.33 25.13 13.33 22.76 11.9 22.76 11.9 19.91 16.1 19.91 18.56 17.47 18.56 5.21 20 5.21 20 3.32 13.33 3.32" fill="#900" />
     </svg>
-
