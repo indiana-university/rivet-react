f2c315f8217cd8d7442eba710c3e5f5d13137ccb
John Hoerr <jhoerr@gmail.com>
2018-08-24 16:43:28 -0400

    Refactor Icon component (#96)

--- src/components/util/RivetIcons.test.tsx
+++ src/components/util/RivetIcons.test.tsx
@@ -7,6 +7,10 @@ describe ("<Icon>", () => {
         const cut = shallow(<Icon name="info" />)
         expect(cut.find('svg')).toHaveLength(1);
     })
+    it("should have a default height of 16", () => {
+        const cut = shallow(<Icon name="info" />)
+        expect(cut.find('svg').prop("height")).toBe("16");
+    })
     it("should pass a custom height", () => {
         const cut = shallow(<Icon name="info" height="24" />)
         expect(cut.find('svg').prop("height")).toBe("24");
