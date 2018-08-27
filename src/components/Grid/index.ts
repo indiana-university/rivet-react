c0db163806048d062c0498fd9d040147ac1c6cd0
John Hoerr <jhoerr@gmail.com>
2018-08-27 10:54:41 -0400

    Add `GridItem` as alternate export for `Col`

--- src/components/Grid/index.ts
+++ src/components/Grid/index.ts
@@ -2,3 +2,4 @@ export { default as Container } from './Container';
 export { default as Row } from './Row';
 export { default as Grid } from './Row';
 export { default as Col } from './Col';
+export { default as GridItem } from './Col';
