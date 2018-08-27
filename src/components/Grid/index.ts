04c6a1bb0e3b29f4c3b0e49b298921c12205224b
John Hoerr <jhoerr@gmail.com>
2018-08-27 10:54:17 -0400

    Rename `Grid` as `Row`

--- src/components/Grid/index.ts
+++ src/components/Grid/index.ts
@@ -1,4 +1,4 @@
 export { default as Container } from './Container';
-export { default as Grid } from './Grid';
-export { default as Row } from './Grid';
+export { default as Row } from './Row';
+export { default as Grid } from './Row';
 export { default as Col } from './Col';
