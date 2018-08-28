14499a0f9cf72a8cfcb3ce407ebd24fbc7d34e46
John Hoerr <jhoerr@gmail.com>
2018-08-28 14:11:59 -0400

    Improve Table docs; reformat

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,12 +5,12 @@ import { rivetize } from '../util/Rivet';
 interface TableProps {
     /** 
      * Optional Rivet style which changes the look and feel of the rendered table.
-     * @see https://rivet.uits.iu.edu/components/page-content/tables/
      */
     variant?: 'stripes' | 'plain'
 }
 
-const Table : React.SFC<TableProps & React.HTMLAttributes<HTMLTableElement>> = ({ children, className, variant, ...attrs }) => {
+const Table : React.SFC<TableProps & React.HTMLAttributes<HTMLTableElement>> = 
+({ children, className, variant, ...attrs }) => {
     const classes = classNames({
         ['rvt-table-plain']: variant === 'plain',
         ['rvt-table-stripes']: variant === 'stripes',
