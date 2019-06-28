c5c8f052dbe2345668ac967607f80c272938caa6
Francis Fernandez <fraferna@iu.edu>
2019-06-28 11:44:01 -0400

    feat(StepIndicator): Add step indicator and step components

---
+++ src/components/Step Indicator/Step.tsx
@@ -0,0 +1,55 @@
+/*
+Copyright (C) 2018 The Trustees of Indiana University
+SPDX-License-Identifier: BSD-3-Clause
+*/
+import * as classNames from 'classnames';
+import * as React from 'react'
+
+
+export interface StepProps {
+    current: boolean;
+    indicator: JSX.Element;
+    label: JSX.Element;
+    screenReaderIndicator: string
+    targetLocation?: string;
+    variant?: 'success' | 'warning' | 'danger';
+}
+
+const indicatorClass = 'rvt-steps__indicator';
+
+const variantClass = (variant) => variant && `${indicatorClass}--${variant}`;
+
+const Step : React.SFC <StepProps & React.HTMLAttributes<HTMLLIElement>> =
+({ indicator, label, screenReaderIndicator, targetLocation, variant, ...attrs}) => {
+    const content = (
+        <>
+            <span className="rvt-steps__label">{label}</span>
+            <span className={classNames(indicatorClass, variantClass(variant))}>
+                <span className="rvt-sr-only">{screenReaderIndicator}</span> {indicator}
+            </span>
+        </>
+    );
+    let wrapper = (
+        <span className="rvt-steps__item-content">
+            {content}
+        </span>
+    );
+    if (targetLocation) {
+        wrapper = (
+            <li className="rvt-steps__item" {...attrs}>
+                <a href={targetLocation} className="rvt-steps__item-content">
+                    {content}
+                </a>
+            </li>
+        );
+    }
+
+    return (
+        <li className="rvt-steps__item" {...attrs}>
+            {wrapper}
+        </li>
+    )
+};
+Step.displayName = 'Step';
+
+export default Step;
