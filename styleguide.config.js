32e361cc0f77ab5e21e7aea9dfd51fa0da46eb4a
Weldon Sams <wjsams@gmail.com>
2019-12-27 14:30:50 -0800

    #210 Add webpackConfig to styleguide.config.js
    Application runs, but only produces a whitepage.

--- styleguide.config.js
+++ styleguide.config.js
@@ -8,17 +8,18 @@ const glob = require('glob');
 module.exports = {
     title: 'Rivet React Style Guide',
     resolver: require('react-docgen').resolver.findAllComponentDefinitions,
-    propsParser: require('react-docgen').withDefaultConfig({
-        // https://github.com/styleguidist/react-styleguidist/issues/1439
-        savePropValueAsString: true,
-        propFilter: (prop, component) =>
-            // skip props with no documentation
-            prop.description.length > 0
-            // skip aria props
-            && prop.name.includes("aria-") === false
-            // skip 'rivetize' props (these are documented separately)
-            && ['className','border','display','hide','margin','padding','typescale'].indexOf(prop.name) === -1
-    }).parse,
+//    propsParser: require('react-docgen').withDefaultConfig({
+//        // https://github.com/styleguidist/react-styleguidist/issues/1439
+//        savePropValueAsString: true,
+//        propFilter: (prop, component) =>
+//            // skip props with no documentation
+//            prop.description.length > 0
+//            // skip aria props
+//            && prop.name.includes("aria-") === false
+//            // skip 'rivetize' props (these are documented separately)
+//            && ['className','border','display','hide','margin','padding','typescale'].indexOf(prop.name) === -1
+//    }).parse,
+    webpackConfig: require('react-scripts/config/webpack.config.js'),
     pagePerSection: true,
     exampleMode: 'expand',
     usageMode: 'expand',
