d8cc500309e447de76356722ff35bdbea46fcbec
Francis Fernandez <fraferna@iu.edu>
2020-02-10 10:30:35 -0500

    chore: Update license library and associated config

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,5 @@
+/*
+Copyright (C) 2018 The Trustees of Indiana University
+SPDX-License-Identifier: BSD-3-Clause
+*/
 /// <reference types="react-scripts" />
--- styleguide.setup.js
+++ styleguide.setup.js
@@ -1,3 +1,7 @@
+/*
+Copyright (C) 2018 The Trustees of Indiana University
+SPDX-License-Identifier: BSD-3-Clause
+*/
 import * as rivet from "./src/components";
 import * as addons from "./src/components/Addons";
 
