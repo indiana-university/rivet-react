aa9cc8cc9d2ba8f97b0c3013bde2894001f0c303
Nubli Kasa <nubli.kasa@gmail.com>
2021-03-22 15:40:39 -0400

    feat(Footer): Footer copyright shows current year (#258)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,14 +7,22 @@ import * as React from 'react';
 import * as Rivet from '../util/Rivet';
 import Icon from '../util/RivetIcons';
 
+interface FooterProps {
+    /**
+     * The Copyright year that appears in the footer.
+     * @see https://rivet.iu.edu/components/navigation/footer/
+     */
+    copyrightYear?: number;
+}
+
 const componentClass = 'rvt-footer';
 const liClass = 'rvt-footer__aux-item';
-
 const footerNavLi = (child, index) =>
     <li key={index} className={liClass}>{child}</li>
 
-const Footer: React.SFC<React.HTMLAttributes<HTMLDivElement>> =
-({ className, children, id = Rivet.shortuid(), ...attrs }) => (
+const Footer: React.FunctionComponent<FooterProps & React.HTMLAttributes<HTMLDivElement>> =
+({ className, children, id = Rivet.shortuid(), copyrightYear = new Date().getFullYear(), ...attrs }) => {
+return (
     <footer id={id} role="contentinfo" className={classNames(componentClass, className)} {...attrs}>
         <div className="rvt-footer__trident">
             <Icon name="trident-footer" />
@@ -25,11 +33,11 @@ const Footer: React.SFC<React.HTMLAttributes<HTMLDivElement>> =
             </li>
             {React.Children.map(children, footerNavLi)}
             <li key="copyright-footer-link" className={liClass}>
-                <a href="https://www.iu.edu/copyright/index.html">Copyright</a> &copy; 2020 The Trustees of <a href="https://www.iu.edu/">Indiana University</a>
+                <a href="https://www.iu.edu/copyright/index.html">Copyright</a> &copy; {copyrightYear} The Trustees of <a href="https://www.iu.edu/">Indiana University</a>
             </li>
         </ul>
     </footer>
-);
+)};
 Footer.displayName = 'Footer';
 
 export default Footer;
