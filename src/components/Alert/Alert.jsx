858bc0580b6b78507585b7acd2b474901d0ecad3
Francis Fernandez <fraferna@iu.edu>
2022-10-14 17:24:22 -0400

    add alert tests and dismissible alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -16,13 +16,13 @@ const Alert = ({title, onDismiss, variant, isOpen = true, id = Rivet.shortuid(),
 	const alertId = id;
 	const titleId = alertId + '-title';
 
-	const headerFragment = () => (<div class="rvt-alert__title" id={titleId}>{title}</div>);
+	const headerFragment = () => (<div className="rvt-alert__title" id={titleId}>{title}</div>);
 
 	const dismissFragment = () =>
 		onDismiss
 			? (
-				<button class="rvt-alert__dismiss" data-rvt-alert-close>
-					<span class="rvt-sr-only">Dismiss this alert</span>
+				<button className="rvt-alert__dismiss" data-rvt-alert-close onClick={onDismiss}>
+					<span className="rvt-sr-only">Dismiss this alert</span>
 					<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
 						<path fill="currentColor" d="M9.41,8l5.29-5.29a1,1,0,0,0-1.41-1.41L8,6.59,2.71,1.29A1,1,0,0,0,1.29,2.71L6.59,8,1.29,13.29a1,1,0,1,0,1.41,1.41L8,9.41l5.29,5.29a1,1,0,0,0,1.41-1.41Z" />
 					</svg>
@@ -45,16 +45,16 @@ const Alert = ({title, onDismiss, variant, isOpen = true, id = Rivet.shortuid(),
 
 Alert.displayName = 'Alert';
 Alert.propTypes = {
-	/** The variant type which determines how the alert is styled */
-	variant: PropTypes.oneOf(['info', 'success', 'warning', 'danger']).isRequired,
-	/** An extremely brief title for the alert */
-	title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
 	/** A unique identifier for the alert */
 	id: PropTypes.string,
-	/** A function that can be called to have side-effects when the alert is dismissed */
-	onDismiss: PropTypes.func,
 	/** Determines whether the alert is displayed or hidden */
-	isOpen: PropTypes.bool
+	isOpen: PropTypes.bool,
+	/** A function that can be called to have side-effects when the alert dismissal button is selected */
+	onDismiss: PropTypes.func,
+	/** An extremely brief title for the alert */
+	title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+	/** The variant type which determines how the alert is styled */
+	variant: PropTypes.oneOf(['info', 'success', 'warning', 'danger']).isRequired
 };
 
 export default Rivet.rivetize(Alert);
---
+++ src/components/Alert/Alert.test.jsx
@@ -0,0 +1,91 @@
+/*
+Copyright (C) 2018 The Trustees of Indiana University
+SPDX-License-Identifier: BSD-3-Clause
+*/
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import React from 'react';
+
+import Alert from './Alert';
+
+describe('<Alert />', () => {
+	const titleText = 'A Test Component';
+
+	describe('Rendering and text', ()=>{
+		it('should render without throwing an error', () => {
+				render(<Alert title={titleText} variant="info" />);
+				expect(screen.getByRole('alert')).toHaveTextContent(titleText);
+			});
+			it('should include alert text', () => {
+				const bodyText = 'This is the message.';
+				render(<Alert title={titleText} variant="info">{bodyText}</Alert>);
+				expect(screen.getByRole('alert')).toHaveTextContent(bodyText);
+			});
+			it('should apply the id', () => {
+				const testId = 'the_id';
+				const cut = render(<Alert title={titleText} variant="info" id={testId} />);
+				expect(screen.getByRole('alert')).toHaveProperty('id', testId);
+			});      
+	})
+
+	describe('Styling', () => {
+		it('should specify style: error', () => {
+			render(<Alert variant="danger" />);
+			expect(screen.getByRole('alert').firstChild.classList.contains('rvt-alert--danger'));
+		});
+		it('should specify style: info', () => {
+			render(<Alert variant="info" />);
+			expect(screen.getByRole('alert').firstChild.classList.contains('rvt-alert--info'));
+		});
+		it('should specify style: warning', () => {
+			render(<Alert variant="warning" />);
+			expect(screen.getByRole('alert').firstChild.classList.contains('rvt-alert--warning'));
+		});
+		it('should specify style: success', () => {
+			render(<Alert variant="success" />);
+			expect(screen.getByRole('alert').firstChild.classList.contains('rvt-alert--success'));
+		});        
+		it('should apply custom style', () => {
+			render(<Alert variant="info" className="rvt-alert--custom" />);
+			expect(screen.getByRole('alert').firstChild.classList.contains('rvt-alert--custom'));
+		});        
+	});
+
+	describe('Visbility', () => {
+			it('is visible by default', () => {
+					render(<Alert title={titleText} variant="success" />);
+					expect(screen.getByRole('alert')).toBeVisible();
+			});        
+			it('can be made visible', () => {
+				render(<Alert title={titleText} variant="success" isOpen />);
+				expect(screen.getByRole('alert')).toBeVisible();
+		});        
+			it('can be made invisible', () => {
+				render(<Alert title={titleText} variant="success" isOpen={false} />);
+				expect(screen.queryAllByRole('alert')).toHaveLength(0);
+			});        
+	})
+
+	describe('Dismiss behavior', ()=> {
+		it('should include dismiss button when dismissible', () => {
+			render(<Alert variant="info" onDismiss={() => {;}} />);
+			expect(screen.getByRole('button')).toBeVisible();
+		});
+		it('should fire dismiss delegate', async () => {
+			let fired = false;
+			const delegate = () => fired = true;
+			render(<Alert variant="info" onDismiss={delegate} />);
+			await user.click(screen.getByRole('button'));
+
+			expect(fired).toEqual(true);
+		});
+		it('the alert should remain visible when dismiss button clicked', async () => {
+			render(<Alert variant="info" onDismiss={() => {;}} />);
+			await user.click(screen.getByRole('button'));
+
+			expect(screen.getByRole('alert')).toBeVisible();
+		});
+	});
+
+});
