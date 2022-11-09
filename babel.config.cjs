4be1bedcafc637c550dab2c065a61be5f4d1d3a6
Francis Fernandez <fraferna@iu.edu>
2022-11-09 16:27:13 -0500

    style: fix whitespace in babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-		presets: [
-				[
-						'@babel/env',
-						{
-								modules: 'auto',
-								useBuiltIns: 'usage',
-								corejs: '3.25.5'
-						}
-				],
-				'@babel/react',
-		]
-}
+  presets: [
+    [
+      "@babel/env",
+      {
+        modules: "auto",
+        useBuiltIns: "usage",
+        corejs: "3.25.5",
+      },
+    ],
+    "@babel/react",
+  ],
+};
