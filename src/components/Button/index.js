f86845b773893b7fdff68a124c52313fd5f66b22
Francis Fernandez <fraferna@iu.edu>
2022-11-10 10:54:04 -0500

    docs: fix styleguide build process (#307)
    Rename module extensions so styleguidist can correctly interpret them. Also fixes a warning regarding the Forms section configuration.

--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-/*
-Copyright (C) 2018 The Trustees of Indiana University
-SPDX-License-Identifier: BSD-3-Clause
-*/
-export { default as Button } from "./Button.jsx";
-export * from "./ButtonGroup.jsx";
-export { default as SegmentedButton } from "./SegmentedButton.jsx";
