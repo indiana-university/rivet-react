72db73501f345a5ede1658bc0b894d44f6478036
ripandey <ripandey299@gmail.com>
2022-11-17 16:36:11 -0500

    WIP - working on DropdownGroup

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -3,4 +3,4 @@ Copyright (C) 2018 The Trustees of Indiana University
 SPDX-License-Identifier: BSD-3-Clause
 */
 export { default as Dropdown } from "./Dropdown.jsx";
-// export { default as DropdownGroup } from "./DropdownGroup";
+export { default as DropdownGroup } from "./DropdownGroup.jsx";
