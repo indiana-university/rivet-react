679dc07646f385a21b4eefe4e0dcf5c28d29a4d1
ripandey <ripandey299@gmail.com>
2022-11-30 13:55:58 -0500

    replaced svg with Icon component

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -16,6 +16,7 @@ import { Button } from "../Button";
 import classNames from "classnames";
 import * as PropTypes from "prop-types";
 import { TestUtils } from "../util/TestUtils";
+import Icon from "../util/RivetIcons";
 
 export const Dropdown = ({
   toggleDropdownOnClickInside = false,
@@ -118,22 +119,7 @@ export const Dropdown = ({
             {label}
           </span>
         )}
-        {/* begin todo - replace with Icon component */}
-        <svg
-          role="img"
-          alt=""
-          className="rvt-m-left-xs"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill="currentColor"
-            d="M8,12.46a2,2,0,0,1-1.52-.7L1.24,5.65a1,1,0,1,1,1.52-1.3L8,10.46l5.24-6.11a1,1,0,0,1,1.52,1.3L9.52,11.76A2,2,0,0,1,8,12.46Z"
-          />
-        </svg>
-        {/* end todo - replace with Icon component */}
+        <Icon name="caret-down" />
       </Button>
       {isOpen && (
         <div className={menuClasses} aria-hidden={!isOpen} role="menu">
