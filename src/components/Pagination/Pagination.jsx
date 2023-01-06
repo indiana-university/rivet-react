d47e3f3184606f6bcc8f46613a7dfd0effedac4e
Andy Hill <athill@iu.edu>
2023-01-06 13:57:36 -0500

    move Pagination.Item back into Pagination, manual documentation of sub-components

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -9,8 +9,6 @@ import * as PropTypes from "prop-types";
 import Icon from "../util/RivetIcons";
 import * as Rivet from "../util/Rivet";
 
-import PaginationItem from "./PaginationItem";
-
 const classPrefix = "rvt-pagination";
 
 /**
@@ -22,13 +20,11 @@ const Pagination = ({
   children,
   ...attrs
 }) => {
-  let subComponentList = Object.keys(Pagination);
+  let subComponentList = ["li", ...Object.keys(Pagination)];
 
-  // let subComponents = React.Children.map(children, (child) =>
-  //     subComponentList.includes(child.type.name)  ? child : null
-  // );
-
-  let subComponents = React.Children.map(children, (child) => child);
+  let subComponents = React.Children.map(children, (child) =>
+    subComponentList.includes(child.type.name) ? child : null
+  );
 
   return (
     <nav role="navigation">
@@ -44,77 +40,102 @@ Pagination.propTypes = {
   id: PropTypes.string,
 };
 
-// export const Item = ({ ariaLabel, children, className, current, disabled = false, id = Rivet.shortuid(), onClick = (e) => { e.preventDefault(); }, ...attrs }) => {
-//     if (disabled) {
-//         return (
-//             <li id={id} className={classNames(`rvt-pagination__item`, className)} aria-label={ariaLabel} {...attrs}>
-//                 { children }
-//             </li>
-//         );
-//     }
-//     return (
-//         <li id={id} className={classNames("rvt-pagination__item", className)} {...attrs}>
-//             <a href="#0" onClick={onClick} aria-label={ariaLabel} aria-current={current && "page"}>
-//                 { children }
-//             </a>
-//         </li>
-//     );
-// };
-// Item.propTypes = {
-//   /** A accessible label to use when using icons in navigation */
-//   ariaLabel: PropTypes.string,
-//   /** Whether this item is the current page */
-//   current: PropTypes.bool,
-//   /** A unique identifier */
-//   id: PropTypes.string,
-//   /** Whether the navigation item is disabled */
-//   disabled: PropTypes.bool,
-//   /** A function to call when the item is clicked */
-//   onClick: PropTypes.func,
-// };
-// Item.displayName = "Pagination.Item";
-// Pagination.Item = Item;
+export const Item = ({
+  ariaLabel,
+  children,
+  className,
+  current,
+  disabled = false,
+  id = Rivet.shortuid(),
+  onClick = (e) => {
+    e.preventDefault();
+  },
+  ...attrs
+}) => {
+  if (disabled) {
+    return (
+      <li
+        id={id}
+        className={classNames(`rvt-pagination__item`, className)}
+        aria-label={ariaLabel}
+        {...attrs}
+      >
+        {children}
+      </li>
+    );
+  }
+  return (
+    <li
+      id={id}
+      className={classNames("rvt-pagination__item", className)}
+      {...attrs}
+    >
+      <a
+        href="#0"
+        onClick={onClick}
+        aria-label={ariaLabel}
+        aria-current={current && "page"}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
+Item.propTypes = {
+  /** A accessible label to use when using icons in navigation */
+  ariaLabel: PropTypes.string,
+  /** Whether this item is the current page */
+  current: PropTypes.bool,
+  /** A unique identifier */
+  id: PropTypes.string,
+  /** Whether the navigation item is disabled */
+  disabled: PropTypes.bool,
+  /** A function to call when the item is clicked */
+  onClick: PropTypes.func,
+};
+Item.displayName = "Pagination.Item";
+Pagination.Item = Item;
 
 const First = ({ ...attrs }) => (
-  <PaginationItem
+  <Pagination.Item
     {...attrs}
     ariaLabel={attrs.disabled ? "No previous pages" : "Go to first page"}
   >
     <Icon name="first" />
-  </PaginationItem>
+  </Pagination.Item>
 );
 First.displayName = "Pagination.First";
 Pagination.First = First;
 
 const Previous = ({ ...attrs }) => (
-  <PaginationItem
+  <Pagination.Item
     {...attrs}
     ariaLabel={attrs.disabled ? "No previous pages" : "Go to previous page"}
   >
     <Icon name="previous" />
-  </PaginationItem>
+  </Pagination.Item>
 );
 Previous.displayName = "Pagination.Previous";
 Pagination.Previous = Previous;
 
 const Next = ({ ...attrs }) => (
-  <PaginationItem
+  <Pagination.Item
     {...attrs}
     ariaLabel={attrs.disabled ? "No next page" : "Go to next page"}
   >
     <Icon name="next" />
-  </PaginationItem>
+  </Pagination.Item>
 );
 Next.displayName = "Pagination.Next";
 Pagination.Next = Next;
 
 const Last = ({ ...attrs }) => (
-  <PaginationItem
+  <Pagination.Item
     {...attrs}
     ariaLabel={attrs.disabled ? "No next page" : "Go to last page"}
   >
     <Icon name="last" />
-  </PaginationItem>
+  </Pagination.Item>
 );
 Last.displayName = "Pagination.Last";
 Pagination.Last = Last;
