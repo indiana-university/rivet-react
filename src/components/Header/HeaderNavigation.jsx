27fe3ee44ea3e1d8dd741c4f70251a07276c7e3e
ripandey <ripandey299@gmail.com>
2023-01-19 13:12:54 -0500

    moved common child rendering methods for Header into childUtils.jsx. Renamed childUtils.ts to childUtils.jsx

--- src/components/Header/HeaderNavigation.jsx
+++ src/components/Header/HeaderNavigation.jsx
@@ -3,11 +3,9 @@ Copyright (C) 2018 The Trustees of Indiana University
 SPDX-License-Identifier: BSD-3-Clause
 */
 import * as React from "react";
-import classNames from "classnames";
-import getDisplayName from "react-display-name";
 
 import Icon, { IconNames } from "../util/RivetIcons";
-import HeaderMenu from "./HeaderMenu";
+import { renderHeaderUnorderedList } from "../util/childUtils";
 
 const HeaderNavigation = ({ children, ...attrs }) => {
   const [isNavMenuOpen, setIsNavMenuOpen] = React.useState(false);
@@ -30,43 +28,10 @@ const HeaderNavigation = ({ children, ...attrs }) => {
         data-rvt-disclosure-target="menu"
         hidden={!isNavMenuOpen}
       >
-        {React.Children.map(children, renderUnorderedList)}
+        {React.Children.map(children, renderHeaderUnorderedList)}
       </nav>
     </div>
   );
-};
-
-const renderListItem = (child) => {
-  const isListItemCurrent =
-    child.props.className &&
-    child.props.className.includes("rvt-header-menu__item--current");
-
-  let childrenWithProps = React.Children.map(child.props.children, (child) => {
-    const childType = child && child["type"];
-    const isHeaderMenu = getDisplayName(childType) === HeaderMenu.displayName;
-    const isLink = childType === "a";
-
-    const headerMenuProps = { ...(isListItemCurrent && { current: true }) };
-    const linkProps = {
-      className: "rvt-header-menu__link",
-      ...(isListItemCurrent && { "aria-current": "page" }),
-    };
-
-    return isHeaderMenu || isLink
-      ? React.cloneElement(child, isHeaderMenu ? headerMenuProps : linkProps)
-      : child;
-  });
-
-  return (
-    <li className={classNames("rvt-header-menu__item", child.props.className)}>
-      {childrenWithProps}
-    </li>
-  );
-};
-
-const renderUnorderedList = (child) => {
-  let listItems = React.Children.map(child.props.children, renderListItem);
-  return <ul className={"rvt-header-menu__list"}>{listItems}</ul>;
 };
 
 HeaderNavigation.displayName = "Header.Navigation";
