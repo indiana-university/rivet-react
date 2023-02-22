5c0330079676284911cbb7583de34c2ae335f0cb
ripandey <ripandey299@gmail.com>
2023-02-22 16:55:31 -0500

    cleaned up EventUtils

--- src/components/util/EventUtils.js
+++ src/components/util/EventUtils.js
@@ -10,54 +10,28 @@ export const keys = {
   arrowDown: "ArrowDown",
 };
 
-export const handler = (callback, InstantiatingClass) => {
-  const eventHandler = (event) => {
-    callback(new InstantiatingClass(event));
-  };
-  return {
-    register: () => {
-      ["click", "touchstart", "keyup"].forEach((event) =>
-        document.addEventListener(event, eventHandler, true)
-      );
-    },
-    deregister: () => {
-      ["click", "touchstart", "keyup"].forEach((event) =>
-        document.removeEventListener(event, eventHandler, true)
-      );
-    },
-  };
-};
+export const isKeyEvent = (event) =>
+  event.type === "keydown" || event.type === "keyup";
 
-export const isKeyEvent = (event) => {
-  return event.type === "keydown" || event.type === "keyup";
-};
+export const isTabKeyPress = (event) =>
+  isKeyEvent(event) && event.key === keys.tab;
 
-export const isTabKeyPress = (event) => {
-  return isKeyEvent(event) && event.key === keys.tab;
-};
-
-export const isEscapeKeyPress = (event) => {
-  return isKeyEvent(event) && event.key === keys.escape;
-};
+export const isEscapeKeyPress = (event) =>
+  isKeyEvent(event) && event.key === keys.escape;
 
 export const isArrowKeyPress = (event) =>
   isArrowUpKeyPress(event) || isArrowDownKeyPress(event);
 
-export const isArrowUpKeyPress = (event) => {
-  return isKeyEvent(event) && event.key === keys.arrowUp;
-};
+export const isArrowUpKeyPress = (event) =>
+  isKeyEvent(event) && event.key === keys.arrowUp;
 
-export const isArrowDownKeyPress = (event) => {
-  return isKeyEvent(event) && event.key === keys.arrowDown;
-};
+export const isArrowDownKeyPress = (event) =>
+  isKeyEvent(event) && event.key === keys.arrowDown;
 
-export const isMouseEvent = (event) => {
-  return event.type === "click";
-};
+export const isMouseEvent = (event) => event.type === "click";
 
-export const isRightMouseClick = (event) => {
-  return isMouseEvent(event) && event.button === 2;
-};
+export const isRightMouseClick = (event) =>
+  isMouseEvent(event) && event.button === 2;
 
 export const targets = (container, event) =>
   container && container.contains(event.target) && container !== event.target;
