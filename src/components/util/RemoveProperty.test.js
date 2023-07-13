af929261f5d4f0bd6ef0408b4e40bf6f37cdb780
Francis Fernandez <fraferna@iu.edu>
2023-07-13 14:55:10 -0400

    chore: add license information
    * Add license file
    * Add license check config
    * Apply license to source files
    * Remove unused static files

--- src/components/util/RemoveProperty.test.js
+++ src/components/util/RemoveProperty.test.js
@@ -1,3 +1,7 @@
+/*
+Copyright (C) 2018 The Trustees of Indiana University
+SPDX-License-Identifier: BSD-3-Clause
+*/
 import "@testing-library/jest-dom";
 import React from "react";
 import { removeProperty } from "./RemoveProperty.js";
