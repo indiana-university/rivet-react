fab5a8553c507238569f7cfb459ca52842e00301
johglove <johglove@iu.edu>
2023-12-20 13:49:56 -0500

    383 - Setup subnav component export

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
         "rivet-react-pagination": resolve(__dirname, "lib/Pagination.js"),
         "rivet-react-radiobutton": resolve(__dirname, "lib/RadioButton.js"),
         "rivet-react-table": resolve(__dirname, "lib/Table.js"),
+        "rivet-react-subnav": resolve(__dirname, "lib/Subnav.js")
       },
       formats: ['es', 'cjs']
     },
