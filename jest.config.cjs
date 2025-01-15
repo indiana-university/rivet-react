36d045fa44a62b9f4e76aacbbf8ef06ab41862b8
Francis Fernandez <fraferna@iu.edu>
2025-01-15 10:18:19 -0500

    fix: use Rivet stickers on layouts (#502)
    Per the responses on rivet-stickers issues [#8](https://github.com/indiana-university/rivet-stickers/issues/8), [#9](https://github.com/indiana-university/rivet-stickers/issues/9), and [#10](https://github.com/indiana-university/rivet-stickers/issues/10), use the default Rivet stickers for those layouts.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: "jsdom",
-  transformIgnorePatterns: ["/node_modules/(?!(rivet\-icons)/)"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(rivet\-icons|rivet\-stickers)/)",
+  ],
   collectCoverage: true,
   reporters: ["default", "jest-junit"],
   coverageReporters: ["text", "cobertura"],
